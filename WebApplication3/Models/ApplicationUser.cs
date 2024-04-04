3e7a6233b402efe5f49d9d6ff1d10e42d0e8c98e
tiandong14 <50548610+tiandong14@users.noreply.github.com>
2024-04-04 22:10:31 +0800

    增加自定义用户类

---
+++ WebApplication3/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    //自定义user类
+    public class ApplicationUser:IdentityUser
+    {
+        public string city { get; set; }
+    }
+}
