ea56bcdb651f774574bc242f8bebeee5a211b4c2
tiandong14 <50548610+tiandong14@users.noreply.github.com>
2024-04-05 15:15:53 +0800

    角色管理基本完成

--- WebApplication3/Controllers/AdminController.cs
+++ WebApplication3/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StudentManagement.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +17,47 @@ namespace StudentManagement.Controllers
             _roleManager = roleManager;
         }
 
-
         [HttpGet]
-        public IActionResult ListRoles() {
+        public IActionResult ListRoles()
+        {
             var roles = _roleManager.Roles;
             return View(roles);
         }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole IdentityRole = new IdentityRole(roleName: model.RoleName);
+                //将角色保存到表里面去
+                IdentityResult Result = await _roleManager.CreateAsync(IdentityRole);
+                if (Result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                foreach (IdentityError error in Result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+            return View(model);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            //todo
+            return View();
+        }
+
     }
 }
---
+++ WebApplication3/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required(ErrorMessage ="角色名不能为空")]
+        [Display(Name = "角色名")]
+        public string RoleName { get; set; }
+    }
+}
